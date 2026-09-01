82940572ca833e23be3550a0fe15f94e7b723143
why769 <15193241329@163.com>
2026-09-01 22:18:38 +0800

    Add files via upload

---
+++ day1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+	char ch = 'A';
+	int age = 20;
+	float score=98.5;
+	double pi = 3.1415926;
+	printf("%c\n", ch);
+	printf("%d\n", age);
+	printf("%f\n", score);
+	printf("%lf\n", pi);
+
+	return 0;
+}	
